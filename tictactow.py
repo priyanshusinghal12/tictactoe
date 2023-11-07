005fe0ee8619fb779374574d10fd774467526ba9
priyanshusinghal12 <150108714+priyanshusinghal12@users.noreply.github.com>
2023-11-07 00:28:36 -0500

    Create tictactow.py

---
+++ tictactow.py
@@ -0,0 +1,48 @@
+def print_board(board):
+    for row in board:
+        print(" | ".join(row))
+        print("-" * 9)
+
+def check_win(board, player):
+    # Check rows, columns, and diagonals for a win
+    for i in range(3):
+        if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
+            return True
+    if all(board[i][i] == player for i in range(3)) or all(board[i][2 - i] == player for i in range(3)):
+        return True
+    return False
+
+def is_full(board):
+    return all(cell != " " for row in board for cell in row)
+
+def main():
+    board = [[" " for _ in range(3)] for _ in range(3)]
+    current_player = "X"
+
+    while True:
+        print_board(board)
+        print(f"Player {current_player}'s turn.")
+        
+        try:
+            row, col = map(int, input("Enter row and column (0-2) separated by space: ").split())
+            if board[row][col] != " ":
+                print("Cell already occupied. Try again.")
+                continue
+            board[row][col] = current_player
+        except (ValueError, IndexError):
+            print("Invalid input. Please enter two numbers in the range 0-2 separated by space.")
+            continue
+
+        if check_win(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        elif is_full(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+
+        current_player = "O" if current_player == "X" else "X"
+
+if __name__ == "__main__":
+    main()
